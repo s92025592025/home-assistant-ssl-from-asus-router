d497ad25a2c5500a1898ecd12dd96c1cff38b85e
Daniel Wang <s92025592025@gmail.com>
2022-02-10 22:33:54 -0800

    add console output

--- run.sh
+++ run.sh
@@ -9,22 +9,30 @@ RSA_PRIVATE_KEY_PATH="$(bashio::config 'sslFromAsusRouter.rsaPrivateKeyPath')"
 KEY_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.keyFilePathOnRouter')"
 CERT_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.certFilePathOnRouter')"
 
+echo "Getting Router Public RSA Key...."
 ROUTER_RSA_KEY=$(ssh-keyscan -t rsa ${ROUTER_IP})
 
+echo "Creating ${SSH_DIR}"
 mkdir -p ${SSH_DIR}
 
+echo "Setting id_rsa file..."
 cp /config/${RSA_PRIVATE_KEY_PATH} ${SSH_DIR}/id_rsa
 chmod 600 ${SSH_DIR}/id_rsa
 
+echo "Touching ${SSH_DIR}/known_hosts..."
 touch ${SSH_DIR}/known_hosts
+
+echo "Setting ${SSH_DIR}/known_hosts Permission..."
 chmod 644 ${SSH_DIR}/known_hosts
 
 ls -lrt ${SSH_DIR}
 
+echo "Saving know hosts..."
 KNOW_HOST_SSH_SEARCH_RESULT=$(grep "${ROUTER_RSA_KEY}" ${SSH_DIR}/known_hosts)
 if [[ -n "$KNOW_HOST_SSH_SEARCH_RESULT" ]]; then
 	echo "$ROUTER_RSA_KEY" >> ${SSH_DIR}/known_hosts;
 fi
 
+echo "scping..."
 scp -v ${ROUTER_USER}@${ROUTER_IP}:/tmp/etc/key.pem /ssl/
 scp -v ${ROUTER_USER}@${ROUTER_IP}:/tmp/etc/cert.pem /ssl/
