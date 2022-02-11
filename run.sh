722890ccbbc1562b57d47f0dfdb5d94c33bd5c4b
Daniel Wang <s92025592025@gmail.com>
2022-02-10 21:33:05 -0800

    use bashio

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/with-contenv bashio
+set -e
 
 ROUTER_USER="$(bashio::config 'sslFromAsusRouter.routerUser')"
 ROUTER_IP="$(bashio::config 'sslFromAsusRouter.routerIp')"
@@ -8,13 +9,14 @@ CERT_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.certFilePathOnRouter')"
 
 ROUTER_RSA_KEY=$(ssh-keyscan -t rsa ${ROUTER_IP})
 
-IF [ -f /data/.ssh/known_hosts ]; THEN
+
+if bashio::fs.file_exists "/data/.ssh/known_hosts"; THEN
 	KNOW_HOST_SSH_SEARCH_RESULT=$(grep "${ROUTER_RSA_KEY}" /data/.ssh/known_hosts)
 
-	IF [[ -n "$KNOW_HOST_SSH_SEARCH_RESULT" ]]; THEN
+	if [[ -n "$KNOW_HOST_SSH_SEARCH_RESULT" ]]; THEN
 		echo $ROUTER_RSA_KEY >> /data/.ssh/known_hosts
-	FI;
-FI;
+	fi
+fi
 
 scp -v -i /data/.ssh/id_rsa -o UserKnownHostsFile=/data/.ssh/known_hosts $ROUTER_USER@$$ROUTER_IP:/tmp/etc/key.pem /ssl/
 scp -v -i /data/.ssh/id_rsa -o UserKnownHostsFile=/data/.ssh/known_hosts $ROUTER_USER@$$ROUTER_IP:/tmp/etc/cert.pem /ssl/
