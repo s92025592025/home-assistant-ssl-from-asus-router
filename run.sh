07d744828dd0636fb2e3bc07a3ba6f7e2c664350
Daniel Wang <s92025592025@gmail.com>
2022-02-10 18:50:52 -0800

    seet config path

--- run.sh
+++ run.sh
@@ -3,8 +3,5 @@
 echo "Hello world!"
 ls -lrt /ssl
 
-echo "CONFIG_PATH: $CONFIG_PATH"
-ls -alrt $CONFIG_PATH
-cat $CONFIG_PATH
 ROUTER_USER="$(bashio::config 'sslFromAsusRouter.routerUser')"
 echo "ROUTER_USER: $ROUTER_USER"
