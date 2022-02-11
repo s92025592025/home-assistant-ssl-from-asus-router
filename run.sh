c3372ccaf8056d7fe57d040ea8d6b02b56b9c3ea
Daniel Wang <s92025592025@gmail.com>
2022-02-10 18:41:58 -0800

    apperantly underscore kills yaml

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ ls -lrt /ssl
 echo "CONFIG_PATH: $CONFIG_PATH"
 ls -alrt $CONFIG_PATH
 cat $CONFIG_PATH
-ROUTER_USER="$(bashio::config 'home_assistant_ssl_from_asus_router.router_user')"
+ROUTER_USER="$(bashio::config 'sslFromAsusRouter.routerUser')"
 echo "ROUTER_USER: $ROUTER_USER"
