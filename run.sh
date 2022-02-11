932639e88f635e5cc8d5401111827ee69569550b
Daniel Wang <s92025592025@gmail.com>
2022-02-10 18:24:15 -0800

    echo more

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 echo "Hello world!"
 ls -lrt /ssl
 
-bashio::log.debug "CONFIG_PATH: $CONFIG_PATH"
+echo "CONFIG_PATH: $CONFIG_PATH"
+ls -alrt $CONFIG_PATH
+cat $CONFIG_PATH
 ROUTER_USER="$(bashio::config 'home_assistant_ssl_from_asus_router.router_user')"
 echo "ROUTER_USER: $ROUTER_USER"
