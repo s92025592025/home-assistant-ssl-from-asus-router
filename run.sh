fbf11ce832becfe0e0b72cef81b744009c150c30
Daniel Wang <s92025592025@gmail.com>
2022-02-10 20:37:41 -0800

    basic scp

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world!"
-ls -lrt /ssl
-
-echo "CONFIG_PATH: $CONFIG_PATH"
-ls -alrt $CONFIG_PATH
-cat $CONFIG_PATH
-
 ROUTER_USER="$(bashio::config 'sslFromAsusRouter.routerUser')"
-echo "ROUTER_USER: $ROUTER_USER"
+ROUTER_IP="$(bashio::config 'sslFromAsusRouter.routerIp')"
+ROUTER_SSH_HOST_KEY="$(bashio::config 'sslFromAsusRouter.routerSshHostKey')"
+KEY_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.keyFilePathOnRouter')"
+CERT_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.certFilePathOnRouter')"
+
+ROUTER_RSA_KEY=$(ssh-keyscan -t rsa ${ROUTER_IP})
+KNOW_HOST_SSH_SEARCH_RESULT=$(grep ${ROUTER_RSA_KEY} ~/.ssh/known_hosts)
+
+IF [[ -n "$KNOW_HOST_SSH_SEARCH_RESULT" ]]; THEN
+	echo $ROUTER_RSA_KEY >> ~/.ssh/known_hosts
+FI;
+
+scp -v $ROUTER_USER@$$ROUTER_IP:/tmp/etc/key.pem /ssl/
+scp -v $ROUTER_USER@$$ROUTER_IP:/tmp/etc/cert.pem /ssl/
