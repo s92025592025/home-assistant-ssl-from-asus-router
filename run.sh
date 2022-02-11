d009672f3e90a3ef67e470b763d1eeb67aa71898
Daniel Wang <s92025592025@gmail.com>
2022-02-10 22:02:42 -0800

    has to provide provate key

--- run.sh
+++ run.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
+SSH_DIR=~/.ssh
+
 ROUTER_USER="$(bashio::config 'sslFromAsusRouter.routerUser')"
 ROUTER_IP="$(bashio::config 'sslFromAsusRouter.routerIp')"
-ROUTER_SSH_HOST_KEY="$(bashio::config 'sslFromAsusRouter.routerSshHostKey')"
+SSH_ACCESS_PRIVATE_RSA_KEY="$(bashio::config 'sslFromAsusRouter.accessSshPrivateRsaKey')"
 KEY_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.keyFilePathOnRouter')"
 CERT_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.certFilePathOnRouter')"
 
 ROUTER_RSA_KEY=$(ssh-keyscan -t rsa ${ROUTER_IP})
 
-ls -lrt /data/.ssh
+mkdir -p ${SSH_DIR}
 
-if bashio::fs.file_exists "/data/.ssh/known_hosts"; then
-	KNOW_HOST_SSH_SEARCH_RESULT=$(grep "${ROUTER_RSA_KEY}" /data/.ssh/known_hosts)
+if bashio::fs.file_exists "${SSH_DIR}/known_hosts"; then
+	KNOW_HOST_SSH_SEARCH_RESULT=$(grep "${ROUTER_RSA_KEY}" ${SSH_DIR}/known_hosts)
 
 	if [[ -n "$KNOW_HOST_SSH_SEARCH_RESULT" ]]; then
-		echo "$ROUTER_RSA_KEY" >> /data/.ssh/known_hosts;
+		echo "$ROUTER_RSA_KEY" >> ${SSH_DIR}/known_hosts;
 	fi
 else
-	mkdir -p /data/.ssh/
-	echo "$(grep "${ROUTER_RSA_KEY}" /data/.ssh/known_hosts)" >> /data/.ssh/known_hosts;
+	
+	echo "$(grep "${ROUTER_RSA_KEY}" ${SSH_DIR}/known_hosts)" >> ${SSH_DIR}/known_hosts;
 fi
 
-scp -v -i /data/.ssh/id_rsa -o UserKnownHostsFile=/data/.ssh/known_hosts ${ROUTER_USER}@${ROUTER_IP}:/tmp/etc/key.pem /ssl/
-scp -v -i /data/.ssh/id_rsa -o UserKnownHostsFile=/data/.ssh/known_hosts ${ROUTER_USER}@${ROUTER_IP}:/tmp/etc/cert.pem /ssl/
+scp -v ${ROUTER_USER}@${ROUTER_IP}:/tmp/etc/key.pem /ssl/
+scp -v ${ROUTER_USER}@${ROUTER_IP}:/tmp/etc/cert.pem /ssl/
