e0da116ae3288096c7132bb59571495c5aa88ee0
Daniel Wang <s92025592025@gmail.com>
2022-02-10 21:16:44 -0800

    use ssh in data

--- run.sh
+++ run.sh
@@ -7,11 +7,14 @@ KEY_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.keyFilePathOnRouter')"
 CERT_PATH_ON_ROUTER="$(bashio::config 'sslFromAsusRouter.certFilePathOnRouter')"
 
 ROUTER_RSA_KEY=$(ssh-keyscan -t rsa ${ROUTER_IP})
-KNOW_HOST_SSH_SEARCH_RESULT=$(grep ${ROUTER_RSA_KEY} ~/.ssh/known_hosts)
 
-IF [[ -n "$KNOW_HOST_SSH_SEARCH_RESULT" ]]; THEN
-	echo $ROUTER_RSA_KEY >> ~/.ssh/known_hosts
+IF [[ -f /data/.ssh/known_hosts ]]
+	KNOW_HOST_SSH_SEARCH_RESULT=$(grep "${ROUTER_RSA_KEY}" /data/.ssh/known_hosts)
+
+	IF [[ -n "$KNOW_HOST_SSH_SEARCH_RESULT" ]]; THEN
+		echo $ROUTER_RSA_KEY >> /data/.ssh/known_hosts
+	FI;
 FI;
 
-scp -v $ROUTER_USER@$$ROUTER_IP:/tmp/etc/key.pem /ssl/
-scp -v $ROUTER_USER@$$ROUTER_IP:/tmp/etc/cert.pem /ssl/
+scp -v -i /data/.ssh/id_rsa -o UserKnownHostsFile=/data/.ssh/known_hosts $ROUTER_USER@$$ROUTER_IP:/tmp/etc/key.pem /ssl/
+scp -v -i /data/.ssh/id_rsa -o UserKnownHostsFile=/data/.ssh/known_hosts $ROUTER_USER@$$ROUTER_IP:/tmp/etc/cert.pem /ssl/
